9c2f17a334cc9a9494708408978a7e8ec3ff4f2a
saileshdwivedy <saileshdwivedy3003@gmail.com>
2025-02-25 15:02:47 -0700

    Reset dev to be empty

--- app.py
+++
@@ -1,6 +0,0 @@
-from fetchers.ticketmaster import TicketmasterFetcher
-
-if __name__ == "__main__":
-    max_events = int(input("Enter the number of events to fetch: "))
-    fetcher = TicketmasterFetcher()
-    fetcher.fetch_events(max_events)

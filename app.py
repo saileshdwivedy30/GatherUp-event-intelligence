44fe856f30d83f0db0cf60b36071906061d5a88b
SaadhviRayasam <saadhvi.rayasam@colorado.edu>
2025-03-17 16:58:06 -0600

    EventBrite scrapper

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from fetchers.ticketmaster import TicketmasterFetcher
+from fetchers.eventbrite import EventbriteFetcher
 
 if __name__ == "__main__":
     max_events = int(input("Enter the number of events to fetch: "))
-    fetcher = TicketmasterFetcher()
-    fetcher.fetch_events(max_events)
+    ticketmaster_fetcher  = TicketmasterFetcher()
+    ticketmaster_fetcher.fetch_events(max_events)
+    eventbrite_fetcher = EventbriteFetcher()
+    eventbrite_fetcher.fetch_events(max_events)

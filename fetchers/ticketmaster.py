50d97463df193a3977bdf812c61a23f8d49ef6ef
SaadhviRayasam <saadhvi.rayasam@colorado.edu>
2025-04-24 19:32:30 -0600

    Ticketmaster API testing for Composer

--- fetchers/ticketmaster.py
+++ fetchers/ticketmaster.py
@@ -9,9 +9,9 @@ from database.duplicate_handler import duplicate_handler
 from dateutil.parser import parse
 
 # Load API keys from .env file
-load_dotenv()
-TICKETMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
-
+#load_dotenv()
+#TICKETMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
+TICKETMASTER_API_KEY = 'tGkhSNOmAAvKRMHb6joV1YjGAzotyj5n'
 
 class TicketmasterFetcher(BaseFetcher):
     """Fetch events from Ticketmaster API."""

3cea34aaccc0d377980503396a83228c7daba177
Sailesh Dwivedy <158533539+saileshdwivedy30@users.noreply.github.com>
2025-04-30 17:25:04 -0600

    Update view_sample_events.py

--- ai_ml/view_sample_events.py
+++ ai_ml/view_sample_events.py
@@ -9,7 +9,7 @@ client = MongoClient(os.getenv("MONGO_URI"))
 db = client["event_data"]
 collection = db["events"]
 
-print("📦 Showing 20 sample events:\n")
+print("Showing 20 sample events:\n")
 for event in collection.find().limit(20):
     pprint.pprint(event)
     print("\n" + "-"*50 + "\n")
